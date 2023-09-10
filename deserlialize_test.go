e4aabff41a967288722925acb6b70ed666ecebde
Hiroaki Nakamura <hnakamur@gmail.com>
2023-09-11 03:12:26 +0900

    Use blackbox function for benchmark valueTryToGoString
    ```
    hnakamur@ggear:~/ghq/github.com/hnakamur/mjingo$ go test -v -run ^$ -bench . -benchmem
    goos: linux
    goarch: amd64
    pkg: github.com/hnakamur/mjingo
    cpu: 13th Gen Intel(R) Core(TM) i7-13700KF
    BenchmarkValueTryToGoString
    BenchmarkValueTryToGoString/methodStr
    BenchmarkValueTryToGoString/methodStr-24                574212116                2.025 ns/op           0 B/op          0 allocs/op
    BenchmarkValueTryToGoString/methodNotStr
    BenchmarkValueTryToGoString/methodNotStr-24             52683022                92.72 ns/op          112 B/op          1 allocs/op
    BenchmarkValueTryToGoString/typeSwitchStr
    BenchmarkValueTryToGoString/typeSwitchStr-24            1000000000               0.8765 ns/op          0 B/op          0 allocs/op
    BenchmarkValueTryToGoString/typeSwitchNotStr
    BenchmarkValueTryToGoString/typeSwitchNotStr-24         43412856                95.56 ns/op          112 B/op          1 allocs/op
    PASS
    ok      github.com/hnakamur/mjingo      11.452s
    hnakamur@ggear:~/ghq/github.com/hnakamur/mjingo$ go test -v -run ^$ -bench . -benchmem
    goos: linux
    goarch: amd64
    pkg: github.com/hnakamur/mjingo
    cpu: 13th Gen Intel(R) Core(TM) i7-13700KF
    BenchmarkValueTryToGoString
    BenchmarkValueTryToGoString/methodStr
    BenchmarkValueTryToGoString/methodStr-24                590627456                2.028 ns/op           0 B/op          0 allocs/op
    BenchmarkValueTryToGoString/methodNotStr
    BenchmarkValueTryToGoString/methodNotStr-24             49439601                93.87 ns/op          112 B/op          1 allocs/op
    BenchmarkValueTryToGoString/typeSwitchStr
    BenchmarkValueTryToGoString/typeSwitchStr-24            1000000000               0.8712 ns/op          0 B/op          0 allocs/op
    BenchmarkValueTryToGoString/typeSwitchNotStr
    BenchmarkValueTryToGoString/typeSwitchNotStr-24         37132551                90.82 ns/op          112 B/op          1 allocs/op
    PASS
    ok      github.com/hnakamur/mjingo      10.463s
    hnakamur@ggear:~/ghq/github.com/hnakamur/mjingo$ go test -v -run ^$ -bench . -benchmem
    goos: linux
    goarch: amd64
    pkg: github.com/hnakamur/mjingo
    cpu: 13th Gen Intel(R) Core(TM) i7-13700KF
    BenchmarkValueTryToGoString
    BenchmarkValueTryToGoString/methodStr
    BenchmarkValueTryToGoString/methodStr-24                588944605                2.018 ns/op           0 B/op          0 allocs/op
    BenchmarkValueTryToGoString/methodNotStr
    BenchmarkValueTryToGoString/methodNotStr-24             51546975                89.59 ns/op          112 B/op          1 allocs/op
    BenchmarkValueTryToGoString/typeSwitchStr
    BenchmarkValueTryToGoString/typeSwitchStr-24            1000000000               0.8794 ns/op          0 B/op          0 allocs/op
    BenchmarkValueTryToGoString/typeSwitchNotStr
    BenchmarkValueTryToGoString/typeSwitchNotStr-24         38311322                94.03 ns/op          112 B/op          1 allocs/op
    PASS
    ok      github.com/hnakamur/mjingo      10.650s
    ```

--- deserlialize_test.go
+++ deserlialize_test.go
@@ -3,28 +3,30 @@ package mjingo
 import "testing"
 
 func BenchmarkValueTryToGoString(b *testing.B) {
+	blackbox := func(s string, err error) {}
+
 	b.Run("methodStr", func(b *testing.B) {
 		val := valueFromString("a string")
 		for i := 0; i < b.N; i++ {
-			valueTryToGoStringWithAsStr(val)
+			blackbox(valueTryToGoStringWithAsStr(val))
 		}
 	})
 	b.Run("methodNotStr", func(b *testing.B) {
 		val := valueFromI64(123)
 		for i := 0; i < b.N; i++ {
-			valueTryToGoStringWithAsStr(val)
+			blackbox(valueTryToGoStringWithAsStr(val))
 		}
 	})
 	b.Run("typeSwitchStr", func(b *testing.B) {
 		val := valueFromString("a string")
 		for i := 0; i < b.N; i++ {
-			valueTryToGoString(val)
+			blackbox(valueTryToGoString(val))
 		}
 	})
 	b.Run("typeSwitchNotStr", func(b *testing.B) {
 		val := valueFromI64(123)
 		for i := 0; i < b.N; i++ {
-			valueTryToGoString(val)
+			blackbox(valueTryToGoString(val))
 		}
 	})
 }
